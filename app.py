f343a5061de3ad8ee2fb4c5eb0d41bd8e967d851
Tiffany Chen <tiffanysta.c@gmail.com>
2017-11-11 22:11:56 -0500

    guess who figured this out? me

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import requests, json
-from flask import Flask, render_template
+from flask import Flask, request, render_template, send_from_directory
+from os.path import join, dirname
+from watson_developer_cloud import TextToSpeechV1
 
-link = "https://api.nasa.gov/planetary/apod?api_key=nvBh1FmpyC4jlCwklqCtpLrajtWXclmokLU5A6pd"
-r = requests.get(link)		#make the API call
-data = r.json()				#convert to a json object
+text_to_speech = TextToSpeechV1(
+    username="fe00ba2c-4f2e-43b8-8c51-88c15160f9cb",
+    password="spYXrqwUwdiU",
+    x_watson_learning_opt_out=True)  # Optional flag
 
 jinjas_in_the_night = Flask(__name__)
 
-@jinjas_in_the_night.route("/")
+@jinjas_in_the_night.route("/", methods = ["GET", "POST"])
 def root():
-    return render_template("template.html", pic_title = data["title"], image = data["url"], info = data["explanation"])
+    return render_template("template.html")
+
+@jinjas_in_the_night.route("/listen", methods = ["POST"])
+def listen():
+	voice(request.form["user"])
+	return send_from_directory("data", filename = "output.mp3")
+
+def voice(text):
+	with open(join(dirname(__file__), 'data/output.mp3'), 'wb') as audio_file:
+		audio_file.write(text_to_speech.synthesize(text, accept='audio/mpeg', voice="en-US_AllisonVoice"))
 
 if __name__ == "__main__":
     jinjas_in_the_night.debug = True
